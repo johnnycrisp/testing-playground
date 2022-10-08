adbfd918c0eb5a22edb30a9e7bf36f426188cd39
Johnny Crisp <johnnycrisp2@gmail.com>
2022-10-08 13:20:21 +0200

    added chessboard js and chess js to dog

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ fetchData('https://dog.ceo/api/breeds/image/random')
 function generateDogImg(data) {
 const dogHtml = `<img src="${data}" alt> <p>Here is your dog</p>
 `;
-// Element.innerHTML = dogHTML;
+Element.innerHTML = dogHTML;
 
 }
 
 
-//Hide and show list items 
+// Hide and show list items 
 
 toggleButton.addEventListener('click', () => {
         if(list.style.display === 'none'){
@@ -60,3 +60,39 @@ list.addEventListener('click', (e) => {
     e.target.parentNode.remove();
 });
 
+
+// // chess.js
+
+// import { Chess } from 'chess.js'
+
+// const chess = new Chess()
+
+// while (!chess.game_over()) {
+//     const moves = chess.moves()
+//     const move = moves[Math.floor(Math.random() * moves.length)]
+//     chess.move(move)
+// }
+// console.log(chess.pgn())
+
+// // chessboard.js
+
+
+// var board = Chessboard('myBoard', 'start')
+// var game = new Chess()
+
+// function makeRandomMove () {
+//   var possibleMoves = game.moves()
+
+//   // exit if the game is over
+//   if (game.game_over()) return
+
+//   var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+//   game.move(possibleMoves[randomIdx])
+//   board.position(game.fen())
+
+//   window.setTimeout(makeRandomMove, 500)
+// }
+
+// board = Chessboard('myBoard', 'start')
+
+// window.setTimeout(makeRandomMove, 500)
